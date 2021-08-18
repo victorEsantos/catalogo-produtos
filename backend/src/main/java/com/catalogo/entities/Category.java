58a492cc0e0c5410fba126e40a777216d8f4936a
victor eduardo <victor.eduardo.sntos@gmail.com>
2021-08-18 20:52:53 -0300

    Lombok + refatoracao Categoria

--- backend/src/main/java/com/catalogo/entities/Category.java
+++ backend/src/main/java/com/catalogo/entities/Category.java
@@ -1,5 +1,8 @@
 package com.catalogo.entities;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.io.Serializable;
 import java.time.Instant;
 import java.util.HashSet;
@@ -17,10 +20,10 @@ import javax.persistence.Table;
 
 @Entity
 @Table(name = "tb_category")
+@Data
+@NoArgsConstructor
 public class Category implements Serializable{
-	
-	// SerialIzable para o objeto ser transformado em bytes
-	
+
 	private static final long serialVersionUID = 1L;
 	
 	@Id
@@ -29,99 +32,27 @@ public class Category implements Serializable{
 	private String name;
 	
 	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
-	private Instant createdAt; // atributo para armezenar o instante que o registro foi criado pela primeira vez
+	private Instant createdAt;
 	
 	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
-	private Instant updateddAt; // atributo para armezenar o instante que o registro foi atualizado
-	
-	// associação bi direcional / uma categoria contem varios produtos / um para muitos
+	private Instant updateddAt;
 	
 	@ManyToMany(mappedBy = "categories")
 	private Set<Product> products = new HashSet<>();
 	
-	// construtor default
-	
-	public Category() {
-		
-	}
-	
-	// construtor com argumento
-
 	public Category(Long id, String name) {
 		this.id = id;
 		this.name = name;
 	}
 	
-	// Getters $ setters
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public Instant getCreatedAt() {
-		return createdAt;
-	}
-
-	public Instant getUpdateddAt() {
-		return updateddAt;
-	}
-	
-	
-	public Set<Product> getProducts() {
-		return products;
-	}
-
-	//metodo auxiliar para sempre que for salvar uma categoria, o metodo armazana no createdAt o instante atual
 	@PrePersist
 	public void prePersist() {
 		createdAt = Instant.now();	
 	}
 	
-	// metodo auxiliar para sempre que for atualizar, o metodo armazena no updatedAt o instante atual
 	@PreUpdate
 	public void preUpdate() {
 		updateddAt = Instant.now();
 	}
 	
-	// HashCode & equals
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Category other = (Category) obj;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		return true;
-	}
-	
-	 
-
 }
