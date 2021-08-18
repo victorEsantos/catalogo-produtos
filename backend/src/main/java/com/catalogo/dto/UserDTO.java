95a9ea03b90e4d7a5d101386fcb097434207292b
victor eduardo <victor.eduardo.sntos@gmail.com>
2021-08-18 20:56:31 -0300

    refatoracao userDTO

--- backend/src/main/java/com/catalogo/dto/UserDTO.java
+++ backend/src/main/java/com/catalogo/dto/UserDTO.java
@@ -8,18 +8,16 @@ import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 
 import com.catalogo.entities.User;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-// objeto para trafegar os dados da aplicação para o controller
-// não vai ser monitorado pela JPA
-
+@Data
+@NoArgsConstructor
 public class UserDTO implements Serializable{
-	
-	// SerialIzable para transformar o objeto em bytes
 	
 	private static final long serialVersionUID = 1L;
 
-	// atributos basico
-	
 	private Long id;
 	
 	@NotBlank(message = "Campo obrigatório")
@@ -29,19 +27,9 @@ public class UserDTO implements Serializable{
 	@Email(message = "Favor entrar com um email válido")
 	private String email;
 	
-	// UserDTO vai transportar uma lista de usuarios e suas permissoes/ Admin, Operador
-	
 	private Set<RoleDTO> roles = new HashSet<>();
- 	
-	
-	// construtor dafault
-	
-	public UserDTO() {
-		
-	}
-	
-	// construtor com argumento
 
+	@Builder
 	public UserDTO(Long id, String firstName, String lastName, String email) {
 		this.id = id;
 		this.firstName = firstName;
@@ -50,8 +38,6 @@ public class UserDTO implements Serializable{
 		
 	}
 	
-	// construtor personalizado recebendo uma entidade
-	
 	public UserDTO(User entity) {
 		this.id = entity.getId();
 		this.firstName = entity.getFirstName();
@@ -59,44 +45,5 @@ public class UserDTO implements Serializable{
 		this.email = entity.getEmail();
 		entity.getRoles().forEach(role -> this.roles.add(new RoleDTO(role)));
 	}
-	
-	// Gatters & setters
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getFirstName() {
-		return firstName;
-	}
-
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-
-	public String getLastName() {
-		return lastName;
-	}
-
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public Set<RoleDTO> getRoles() {
-		return roles;
-	}
-	
 	
 }
