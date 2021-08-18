80781839fbffc5c0d8195dc3f0efd0c37038ad1f
victor eduardo <victor.eduardo.sntos@gmail.com>
2021-08-18 20:54:46 -0300

    refatoracao user

--- backend/src/main/java/com/catalogo/entities/User.java
+++ backend/src/main/java/com/catalogo/entities/User.java
@@ -1,5 +1,9 @@
 package com.catalogo.entities;
 
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.io.Serializable;
 import java.util.HashSet;
 import java.util.Set;
@@ -15,17 +19,13 @@ import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
-// entidade
-
-
 @Entity
 @Table(name = "tb_user")
+@Data
+@NoArgsConstructor
 public class User implements Serializable{	
-	// para que o objeto seja transformado em bytes	
 	private static final long serialVersionUID = 1L;
 
-	// atributos basico
-	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;	
@@ -37,22 +37,13 @@ public class User implements Serializable{
 	
 	private String password;
 	
-	// associação / um usuario pode ter varios perfis / um ou muitos
-	
 	@ManyToMany(fetch = FetchType.EAGER)
 	@JoinTable(name = "tb_user_role", 
 	joinColumns = @JoinColumn(name = "user_id"),
 	inverseJoinColumns = @JoinColumn(name = "role_id"))
 	private Set<Role> roles = new HashSet<>();
-	
-	// construtor default
-	
-	public User() {
-		
-	}
-	
-	// construtor com argumentos
 
+	@Builder
 	public User(Long id, String firstName, String lastName, String email, String password) {
 		this.id = id;
 		this.firstName = firstName;
@@ -60,82 +51,5 @@ public class User implements Serializable{
 		this.email = email;
 		this.password = password;
 	}
-	
-	// Getters & setters
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getFirstName() {
-		return firstName;
-	}
-
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-
-	public String getLastName() {
-		return lastName;
-	}
-
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	
-	
-	public Set<Role> getRoles() {
-		return roles;
-	}
-	
-	// HashCode & equals
-
-	
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		User other = (User) obj;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		return true;
-	}
-	
 	
 }
