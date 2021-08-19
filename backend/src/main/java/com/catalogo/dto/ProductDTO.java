664ce114d1f717edad5b7a071a675d1b1c69d805
victor eduardo <victor.eduardo.sntos@gmail.com>
2021-08-18 21:05:08 -0300

    refatoracao produto

--- backend/src/main/java/com/catalogo/dto/ProductDTO.java
+++ backend/src/main/java/com/catalogo/dto/ProductDTO.java
@@ -13,12 +13,14 @@ import javax.validation.constraints.Size;
 
 import com.catalogo.entities.Category;
 import com.catalogo.entities.Product;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@NoArgsConstructor
 public class ProductDTO implements Serializable{
 	private static final long serialVersionUID = 1L;
-	
-	
-	// atributos basico
 	
 	private Long id;
 	
@@ -34,18 +36,9 @@ public class ProductDTO implements Serializable{
 	@PastOrPresent(message = "A data do produto não pode ser futura")
 	private Instant date;
 	
-	// ProductDTO vai aceitar uma lista de categorias
-	
 	private List<CategoryDTO> categories = new ArrayList<>();
-	
-	// construtor default
-	
-	public ProductDTO() {
-		
-	}
-	
-	// construtor com argumentos
 
+	@Builder
 	public ProductDTO(Long id, String name, String description, Double price, String imgUrl, Instant date) {
 		this.id = id;
 		this.name = name;
@@ -54,8 +47,6 @@ public class ProductDTO implements Serializable{
 		this.imgUrl = imgUrl;
 		this.date = date;
 	}
-	
-	// construtor recebendo uma entidade para povoar o dto
 	
 	public ProductDTO(Product entity) {
 		this.id = entity.getId();
@@ -66,69 +57,9 @@ public class ProductDTO implements Serializable{
 		this.date = entity.getDate();
 	}
 	
-	// construtor recebendo as categorias e colocando os elementos 
-	
 	public ProductDTO(Product entity, Set<Category> categories) {
 		this(entity);
 		categories.forEach(x -> this.categories.add(new CategoryDTO(x)));
 	}
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getDescription() {
-		return description;
-	}
-
-	public void setDescription(String description) {
-		this.description = description;
-	}
-
-	public Double getPrice() {
-		return price;
-	}
-
-	public void setPrice(Double price) {
-		this.price = price;
-	}
-
-	public String getImgUrl() {
-		return imgUrl;
-	}
-
-	public void setImgUrl(String imgUrl) {
-		this.imgUrl = imgUrl;
-	}
-
-	public Instant getDate() {
-		return date;
-	}
-
-	public void setDate(Instant date) {
-		this.date = date;
-	}
-
-	public List<CategoryDTO> getCategories() {
-		return categories;
-	}
-
-	public void setCategories(List<CategoryDTO> categories) {
-		this.categories = categories;
-	}
-	
-	
 
 }
--- backend/src/main/java/com/catalogo/entities/Product.java
+++ backend/src/main/java/com/catalogo/entities/Product.java
@@ -1,5 +1,9 @@
 package com.catalogo.entities;
 
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.io.Serializable;
 import java.time.Instant;
 import java.util.HashSet;
@@ -17,13 +21,11 @@ import javax.persistence.Table;
 
 @Entity
 @Table(name = "tb_product")
+@Data
+@NoArgsConstructor
 public class Product implements Serializable{
 	
-	// SerialIzable para o objeto ser transformado em bytes
-	
 	private static final long serialVersionUID = 1L;
-	
-	// atributos basico
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -39,18 +41,13 @@ public class Product implements Serializable{
 	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
 	private Instant date;
 	
-	// associação bi direcional / um produto tem varias categorias / um para muitos
-	
 	@ManyToMany
-	@JoinTable(name = "tb_product_category", 
+	@JoinTable(name = "tb_product_category",
 	joinColumns = @JoinColumn(name = "product_id"),
 	inverseJoinColumns = @JoinColumn(name = "category_id"))
 	private Set<Category> categories = new HashSet<>();
-	
-	public Product() {
-		
-	}
 
+	@Builder
 	public Product(Long id, String name, String description, Double price, String imgUrl, Instant date) {
 		this.id = id;
 		this.name = name;
@@ -60,83 +57,4 @@ public class Product implements Serializable{
 		this.date = date;
 	}
 
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getDescription() {
-		return description;
-	}
-
-	public void setDescription(String description) {
-		this.description = description;
-	}
-
-	public Double getPrice() {
-		return price;
-	}
-
-	public void setPrice(Double price) {
-		this.price = price;
-	}
-
-	public String getImgUrl() {
-		return imgUrl;
-	}
-
-	public void setImgUrl(String imgUrl) {
-		this.imgUrl = imgUrl;
-	}
-
-	public Instant getDate() {
-		return date;
-	}
-
-	public void setDate(Instant date) {
-		this.date = date;
-	}
-
-	public Set<Category> getCategories() {
-		return categories;
-	}
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Product other = (Product) obj;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		return true;
-	}
-
-	
-	
 }
