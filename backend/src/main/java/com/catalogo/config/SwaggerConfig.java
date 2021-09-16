df02a4b6dd2e9260e1c51e58b1d70fe9d32fcdcc
victor eduardo <victor.eduardo.sntos@gmail.com>
2021-09-15 22:30:42 -0300

    enable_swagger

--- backend/src/main/java/com/catalogo/config/SwaggerConfig.java
+++ backend/src/main/java/com/catalogo/config/SwaggerConfig.java
@@ -1,5 +1,6 @@
 package com.catalogo.config;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
@@ -13,6 +14,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
+@ConditionalOnProperty(name="app.api.swagger.enable", havingValue = "true", matchIfMissing = false)
 public class SwaggerConfig {
     @Bean
     public Docket api() {
