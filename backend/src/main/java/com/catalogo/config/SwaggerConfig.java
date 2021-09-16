c2c58a7c51fbbbde3391b3b100ae5d0765c5c644
victor eduardo <victor.eduardo.sntos@gmail.com>
2021-09-15 21:53:25 -0300

    add_swagger_config

---
+++ backend/src/main/java/com/catalogo/config/SwaggerConfig.java
@@ -0,0 +1,37 @@
+package com.catalogo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.catalogo.api.controller"))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("API - Documentação catalogo equipe DEVOK")
+                .description("Documentação simples de um catalogo de produtos")
+                .version("1.0.0")
+                .license("Apache License Version 2.0")
+                .licenseUrl("https://www.apache.org/licenses/LICENSE-2.0")
+                .contact(new Contact("Equipe: Alexandre, Nathã e Victor", "https://github.com/victorEsantos/catalogo-produtos/tree/master/backend", "mail"))
+                .build();
+    }
+}
